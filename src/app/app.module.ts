e52185015bed0927d71b4260d8e0e61813315f22
alonestrak <sushantsawant865@gmail.com>
2021-08-16 16:09:34 +0530

    updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,19 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatTableModule} from '@angular/material/table';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import {MatTabsModule} from '@angular/material/tabs';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { HttpClientModule } from "@angular/common/http";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatTableModule } from "@angular/material/table";
+import { MatPaginatorModule } from "@angular/material/paginator";
+import { MatTabsModule } from "@angular/material/tabs";
+import { FormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
-import { ApiComponentComponent } from './api-component/api-component.component';
-import { FormComponentComponent } from './form-component/form-component.component';
-import { ApiResponseService } from './api-response.service';
-
+import { AppComponent } from "./app.component";
+import { ApiComponentComponent } from "./api-component/api-component.component";
+import { FormComponentComponent } from "./form-component/form-component.component";
+//import { ApiResponseService } from './api-response.service';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ApiComponentComponent,
-    FormComponentComponent
-  ],
+  declarations: [AppComponent, ApiComponentComponent, FormComponentComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
@@ -31,4 +26,4 @@ import { ApiResponseService } from './api-response.service';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/form-component/form-component.component.ts
+++ src/app/form-component/form-component.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-import {Employee} from 'src/app/models/employee';
+import { Component } from "@angular/core";
+import { Employee } from "src/app/models/employee";
 
 @Component({
-  selector: 'app-form-component',
-  templateUrl: './form-component.component.html',
-  styleUrls: ['./form-component.component.css']
+  selector: "app-form-component",
+  templateUrl: "./form-component.component.html",
+  styleUrls: ["./form-component.component.css"]
 })
 export class FormComponentComponent {
-  employeeModel = new Employee ( '' , '' , 1234567890 , (new Date()).toISOString().substring(0,10), '');
+  employeeModel = new Employee(
+    "",
+    "",
+    1234567890,
+    new Date().toISOString().substring(0, 10),
+    ""
+  );
+  alert: boolean = false;
 
-  onSubmit(){
+  onSubmit() {
+    this.alert = true;
     console.log(this.employeeModel);
+  }
+  closeAlert() {
+    this.alert = false;
   }
 }
